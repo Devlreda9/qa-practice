cdf35c6f68673c10afa2659d6b09f5e6950fec52
Reda ARBAOUI <devlreda9@gmail.com>
2026-04-27 12:05:50 +0100

    adding js code

---
+++ qa.js
@@ -0,0 +1,9 @@
+
+function showMessage() {
+    const message = document.getElementById("message");
+
+    message.innerText = "You clicked the button! 🎉";
+
+    message.style.color = "blue";
+    message.style.fontSize = "20px";
+}
